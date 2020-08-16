35c04e6c6a672756bd949bcb1d4d970b5915a8bb
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2020-08-16 22:10:48 +0700

    commit Token Line Comparator

--- src/test/java/ramdan/file/line/token/data/LineTokenComparatorTest.java
+++ src/test/java/ramdan/file/line/token/data/LineTokenComparatorTest.java
@@ -18,6 +18,15 @@ public class LineTokenComparatorTest {
     }
 
     @Test
+    public void compare3(){
+        val comparator = new LineTokenComparator(0,1,2);
+        val lt1 = LineTokenData.newInstance("1","2","3");
+        val lt2 = LineTokenData.newInstance("1","2","3");
+
+        Assert.assertEquals(0,comparator.compare(lt1,lt1));
+        Assert.assertEquals(0,comparator.compare(lt1,lt2));
+    }
+    @Test
     public void compareNull(){
         val comparator = new LineTokenComparator(0);
 
@@ -36,12 +45,19 @@ public class LineTokenComparatorTest {
 
         Assert.assertTrue(comparator.compare(lt1,lt2)<0);
     }
-
     @Test
-    public void compare2(){
+    public void compare_1_2_2_result_negatif(){
         val comparator = new LineTokenComparator(0);
-        val lt1 = LineTokenData.newInstance("2");
-        val lt2 = LineTokenData.newInstance("1");
+        val lt1 = LineTokenData.newInstance("1");
+        val lt2 = LineTokenData.newInstance("2");
+
+        Assert.assertTrue(comparator.compare(lt1,lt2)<0);
+    }
+    @Test
+    public void compare_Resutl_Positif(){
+        val comparator = new LineTokenComparator(0,1,2);
+        val lt1 = LineTokenData.newInstance("1","2","2");
+        val lt2 = LineTokenData.newInstance("1","2","1");
 
         Assert.assertTrue(comparator.compare(lt1,lt2)>0);
     }
