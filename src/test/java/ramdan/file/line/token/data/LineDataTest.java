22bd4cb12e15acb82b0dae67a6b2e34d0707d85d
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2020-08-16 22:11:59 +0700

    commit LineDataTest

--- src/test/java/ramdan/file/line/token/data/LineDataTest.java
+++ src/test/java/ramdan/file/line/token/data/LineDataTest.java
@@ -1,5 +1,6 @@
 package ramdan.file.line.token.data;
 
+import lombok.val;
 import org.junit.Test;
 import ramdan.file.line.token.StringUtils;
 
@@ -11,93 +12,88 @@ public class LineDataTest {
 
     @Test
     public void equal(){
-        assertTrue(StringUtils.equal("data","data"));
-        assertTrue(StringUtils.equal(null,(String)null));
+        val ld = new LineData("data");
+
+        assertTrue(ld.equal("data"));
     }
 
     @Test
     public void equal_negatif(){
-        assertFalse(StringUtils.equal("data","Data"));
-        assertFalse(StringUtils.equal("data",(String)null));
-        assertFalse(StringUtils.equal(null,"Data"));
-    }
+        val ld = new LineData("data");
 
-    @Test
-    public void equal_NotEqual(){
-        LineTokenData lineToken = LineTokenData.newInstance("token-0-data");
-
-        assertFalse(lineToken.equal(0,"salah"));
+        assertFalse(ld.equal("Data"));
     }
 
     @Test
     public void equalIgnoreCase(){
-        assertTrue(StringUtils.equalIgnoreCase("token-0-data","Token-0-data"));
-        assertTrue(StringUtils.equalIgnoreCase("token-0-data","salah","Token-0-data"));
+        val ld = new LineData("token-0-data");
+
+        assertTrue(ld.equalIgnoreCase("Token-0-data"));
+
     }
 
     @Test
     public void equalIgnoreCase_NotEqual(){
-        assertFalse(StringUtils.equalIgnoreCase("token-0-data","salah"));
-    }
+        val ld = new LineData("token-0-data");
 
-    @Test
-    public void testEqualIgnoreCase(){
-        assertTrue(StringUtils.equalIgnoreCase("data","data"));
-        assertTrue(StringUtils.equalIgnoreCase("data","Data"));
-        assertTrue(StringUtils.equalIgnoreCase(null,(String)null));
-    }
-
-    @Test
-    public void testEqualIgnoreCase_negatif(){
-        assertFalse(StringUtils.equalIgnoreCase("data","data2"));
-        assertFalse(StringUtils.equalIgnoreCase("data",(String)null));
-        assertFalse(StringUtils.equalIgnoreCase(null,"Data"));
+        assertFalse(ld.equalIgnoreCase("Token-0-salah"));
     }
 
     @Test
     public void contain_true(){
-        assertTrue(StringUtils.contain("NO IN","data","NO"));
+        val ld = new LineData("NO IN");
+
+        assertTrue(ld.contain("data","NO"));
     }
 
     @Test
     public void contain_false(){
-        assertFalse(StringUtils.contain("NO IN","in","no"));
+        val ld = new LineData("NO IN");
+
+        assertFalse(ld.equalIgnoreCase("in","no"));
     }
 
     @Test
     public void containAll_true(){
-        assertTrue(StringUtils.containAll("NO IN","IN","NO"));
+        val ld = new LineData("NO IN");
+
+        assertTrue(ld.containAll("IN","NO"));
     }
 
     @Test
     public void containAll_false(){
-        assertFalse(StringUtils.containAll("NO IN","no","in"));
+        val ld = new LineData("NO IN");
+
+        assertFalse(ld.containAll("no","in"));
     }
 
     @Test
     public void containAllIgnoreCase_true(){
-        LineTokenData lineToken = LineTokenData.newInstance("NO IN");
+        val ld = new LineData("NO IN");
 
-        assertTrue(lineToken.containAllIgnoreCase(0,"in","no"));
+        assertTrue(ld.containAllIgnoreCase("no","in"));
     }
 
     @Test
     public void containAllIgnoreCase_false(){
-        assertFalse(StringUtils.containAllIgnoreCase("NO IN","no","net"));
+        val ld = new LineData("NO IN");
+
+        assertFalse(ld.containAllIgnoreCase("no","net"));
     }
 
     @Test
     public void containIgnoreCase_true(){
-        LineTokenData lineToken = LineTokenData.newInstance("NO IN");
+        val ld = new LineData("NO IN");
 
-        assertTrue(lineToken.containIgnoreCase(0,"in","no","not","found"));
+        assertTrue(ld.containIgnoreCase("in","no","not","found"));
+
     }
 
     @Test
     public void containIgnoreCase_false(){
-        LineTokenData lineToken = LineTokenData.newInstance("NO IN");
+        val ld = new LineData("NO IN");
 
-        assertFalse(lineToken.containIgnoreCase(0,"not","found"));
+        assertFalse(ld.containIgnoreCase("not","found"));
     }
 
 }
